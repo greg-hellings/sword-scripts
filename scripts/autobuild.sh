9429c57c72e35552969cdc192f622849a6d3c612
Gregory Hellings <greg.hellings@gmail.com>
2017-11-02 08:23:36 -0500

    Abandon usrinst.sh

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 
-./usrinst.sh "--prefix=${WORKSPACE}/destination"
+./configure "--prefix=${WORKSPACE}/destination" \
+	"--sysconfdir=${WORKSPACE}/destination/etc" \
+	--without-conf \
+	--disable-shared \
+	--enable-debug
 make -j4
 make install
