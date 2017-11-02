93d0cdecb4355bd19ea1463085b0d3d54e135388
Gregory Hellings <greg.hellings@gmail.com>
2017-11-02 08:55:22 -0500

    Autotools still can't build outside the dir

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -ex
 
-mkdir -p ../autobuild
-cd ../autobuild
-../sword/configure "--prefix=${WORKSPACE}/${FLAVOR}-destination" \
+./configure "--prefix=${WORKSPACE}/${FLAVOR}-destination" \
 	"--sysconfdir=${WORKSPACE}/${FLAVOR}-destination/etc" \
 	--without-conf \
 	--disable-shared \
