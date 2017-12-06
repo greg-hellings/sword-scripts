1f6cd0f1f83364dbb1ba8654c657ba0551dda6de
Gregory Hellings <greg.hellings@gmail.com>
2017-12-05 21:41:50 -0600

    Weird syntax error with underscores?!

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -ex
 
-cp "${targetDirectory}.tar.gz" "${RELEASE_DIR}/v${MAJOR_VERSION}.${MINOR_VERSION}"
+cp "${targetDirectory}.tar.gz" "${RELEASE_DIR}/v${majorVersion}.${minorVersion}"
 MD5="$(md5sum "${targetDirectory}.tar.gz")"
 SHA512="$(sha512sum "${targetDirectory}.tar.gz")"
 
-cd "${RELEASE_DIR}/v${MAJOR_VERSION}.${MINOR_VERSION}"
+cd "${RELEASE_DIR}/v${majorVersion}.${minorVersion}"
 find . -depth 1 -type d -print0 | while read -d $'\0' directory; do rm -rf "${directory}"; done
 tar xaf "${targetDirectory}.tar.gz"
 cd ..
 find . -depth 1 -type l -print0 | while read -d $'\0' link; do rm -f "${link}"; done
-ln -s "v${MAJOR_VERSION}.${MINOR_VERSION}/${targetDirectory}"
+ln -s "v${majorVersion}.${minorVersion}/${targetDirectory}"
 rm sword.tar.gz
-ln -s "v${MAJOR_VERSION}.${MINOR_VERSION}/${targetDirectory}.tar.gz" sword.tar.gz
+ln -s "v${majorVersion}.${minorVersion}/${targetDirectory}.tar.gz" sword.tar.gz
 
 echo "${MD5}\n${SHA512}"
 echo "http://crosswire.org/sword/$(basename "$(dirname "$(pwd)")")/$(basename "$(pwd)")/${targetDirectory}.tar.gz"
