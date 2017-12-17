57940ff11a78d7505323cc56018c80a877b32bcb
Gregory Hellings <greg.hellings@gmail.com>
2017-12-16 23:55:26 -0600

    Use the correct arguments to find

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,10 +5,10 @@ MD5="$(md5sum "${targetDirectory}.tar.gz")"
 SHA512="$(sha512sum "${targetDirectory}.tar.gz")"
 
 cd "${RELEASE_DIR}/v${majorVersion}.${minorVersion}"
-find . -depth 1 -type d -print0 | while read -d $'\0' directory; do rm -rf "${directory}"; done
+find . -maxdepth 1 -type d -print0 | while read -d $'\0' directory; do rm -rf "${directory}"; done
 tar xaf "${targetDirectory}.tar.gz"
 cd ..
-find . -depth 1 -type l -print0 | while read -d $'\0' link; do rm -f "${link}"; done
+find . -maxdepth 1 -type l -print0 | while read -d $'\0' link; do rm -f "${link}"; done
 ln -s "v${majorVersion}.${minorVersion}/${targetDirectory}"
 rm -f sword.tar.gz
 ln -s "v${majorVersion}.${minorVersion}/${targetDirectory}.tar.gz" sword.tar.gz
