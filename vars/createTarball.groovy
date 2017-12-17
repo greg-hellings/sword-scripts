3d0e35da8b9e81acab4ee35bd00a0fec43f6fa6d
Gregory Hellings <greg.hellings@gmail.com>
2017-12-16 23:40:20 -0600

    Force the debugging output

--- vars/createTarball.groovy
+++ vars/createTarball.groovy
@@ -2,7 +2,7 @@ def call() {
 	cleanWs()
 	sh "svn export '${urlBase}' '${targetDirectory}'"
 	dir("${targetDirectory}/scripts") {
-		sh "set -v && ./mkswordtar"
+		sh "/bin/bash -x mkswordtar"
 	}
 	stash name: "build", includes: "${targetDirectory}.tar.gz"
 }
