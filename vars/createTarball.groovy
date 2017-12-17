68f354a828c1f1f7acaff2e58e925d5a4a0c3fb2
Gregory Hellings <greg.hellings@gmail.com>
2017-12-16 23:36:36 -0600

    Proper set, instead of unset

--- vars/createTarball.groovy
+++ vars/createTarball.groovy
@@ -2,7 +2,7 @@ def call() {
 	cleanWs()
 	sh "svn export '${urlBase}' '${targetDirectory}'"
 	dir("${targetDirectory}/scripts") {
-		sh "set +ex && ./mkswordtar"
+		sh "set -ex && ./mkswordtar"
 	}
 	stash name: "build", includes: "${targetDirectory}.tar.gz"
 }
