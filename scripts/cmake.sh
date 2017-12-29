711714c80f1c8c2b4ce9a0ac5a57e9b2e248efcb
Gregory Hellings <greg.hellings@gmail.com>
2017-12-29 16:49:05 -0600

    Add Python bindings test
    Adds building Python bindings to CMake build

--- scripts/cmake.sh
+++ scripts/cmake.sh
@@ -6,6 +6,7 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX="${WORKSPACE}/${FLAVOR}-destination" \
 	-DLIBSWORD_LIBRARY_TYPE="Static" \
+	-DSWORD_BINDINGS="Python" \
 	..
 make -j4
 make install
