9bed90b9e5cc59c43bac93d8dd469d15d8ed987c
Gregory Hellings <greg.hellings@gmail.com>
2018-01-03 23:46:49 -0600

    Enable bindings in trunk

--- scripts/cmake.sh
+++ scripts/cmake.sh
@@ -6,6 +6,7 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX="${WORKSPACE}/${FLAVOR}-destination" \
 	-DLIBSWORD_LIBRARY_TYPE="Static" \
+	-DSWORD_BINDINGS="Python" \
 	-DSWORD_ENABLE_TESTS="Yes" \
 	..
 make -j8
