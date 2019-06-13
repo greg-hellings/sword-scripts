50c75e592110d4d3cbd176eedcdbcd1510ba6d03
Gregory Hellings <greg.hellings@gmail.com>
2019-06-13 15:21:15 -0500

    New way of building bindings

--- scripts/cmake.sh
+++ scripts/cmake.sh
@@ -6,7 +6,9 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX="${WORKSPACE}/${FLAVOR}-destination" \
 	-DLIBSWORD_LIBRARY_TYPE="Static" \
-	-DSWORD_BINDINGS="Python" \
+	-DSWORD_PYTHON_3:BOOL=TRUE \
+	-DSWORD_PYTHON_2:BOOL=TRUE \
+	-DSWORD_PERL:BOOL=TRUE \
 	-DSWORD_BUILD_TESTS="Yes" \
 	..
 make -j8
